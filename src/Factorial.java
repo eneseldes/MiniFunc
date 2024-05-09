c19ed0867530f1ab6a82268489ef910758c61691
tunahancoban <tunahancbnn@gmail.com>
2024-05-09 14:18:10 +0300

    Added Factorial

---
+++ src/Factorial.java
@@ -0,0 +1,34 @@
+public class Factorial extends ArithmeticUnaryExpression{
+
+    public Factorial(Expression expression) {//Tam sayı alma olayını yap
+        super(expression);
+    }
+    
+
+    @Override
+    Object getValue() {
+        return execute().getValue();
+    }
+
+    @Override
+    Expression execute() {
+        if(expression.getValue() instanceof Integer){
+            if((int)expression.getValue()>1){
+               return new Multipication(expression, new Factorial(new IntegerLiteral((int)expression.getValue()-1)).execute());
+            }
+            else{
+                return new IntegerLiteral(1);
+            }
+        }
+        else if(expression.getValue() instanceof Double){
+            if((double)expression.getValue()>1){
+               return new Multipication(expression,new Factorial(new DoubleLiteral((double)expression.getValue()-1)).execute());
+            }
+            else{
+                return new DoubleLiteral(1.0);
+            }
+        }   
+        return null;//Hata fırlat
+    }
+
+}
